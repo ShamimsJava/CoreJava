f48cf72614055b669567eb3e33d219c0a7a3ff04
shamimsjava <shamimsjava@gmail.com>
2017-08-25 23:35:01 +0600

    Working with operator

--- CoreJava/src/module1/TestGreeting.java
+++ CoreJava/src/module1/TestGreeting.java
@@ -1,9 +1,5 @@
 package module1;
 
-/**
- *
- * @author Shamim Sarker
- */
 public class TestGreeting {
   public static void main(String[] args){
       Greeting hello = new Greeting();
---
+++ CoreJava/src/practice/Operators.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class Operators {
+   static public void main(String args[]){
+       
+       //Java Unary Operator Example: ++ and --
+       int x = 10;
+       System.out.println(x++);
+       System.out.println(++x);
+       System.out.println(x--);
+       System.out.println(--x);
+       
+       int a = 10;
+       int b = 10;
+       System.out.println(a++ + ++a); // print 22
+       System.out.println(b++ + b++); // print 21
+   } 
+}
+
+/*
+ There are many types of operators in java which are given below:
+
+    Unary Operator,
+    Arithmetic Operator,
+    shift Operator,
+    Relational Operator,
+    Bitwise Operator,
+    Logical Operator,
+    Ternary Operator and
+    Assignment Operator.
+*/
