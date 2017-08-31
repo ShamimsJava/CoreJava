94db60763f2cfed5bf9c710b4588912167efb669
shamimsjava <shamimsjava@gmail.com>
2017-08-31 08:48:57 +0600

    Only for practice

---
+++ Practice/A.java
@@ -0,0 +1,8 @@
+class A{
+	int a = 40;
+	public static void main(String[] args){
+		System.out.println(a);
+	}
+}
+
+// to see compile time error. static method can not use non static data member.
---
+++ Practice/A3.java
@@ -0,0 +1,8 @@
+class A3{
+	static{
+		System.out.println("Static block is invoked.");
+		System.exit(0);
+	}
+}
+
+// it is not executable in latest jdk 
