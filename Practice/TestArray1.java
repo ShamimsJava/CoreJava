4466321f2ee9af034f70bdaeb49cbfbc38133d6b
shamimsjava <shamimsjava@gmail.com>
2017-09-01 07:16:45 +0600

    Starting work with Java Array

---
+++ Practice/TestArray1.java
@@ -0,0 +1,8 @@
+class TestArray1{
+	public static void main(String[] args){
+		int a[] = {10,20,30,40,50};
+		for(int i=0; i<a.length; i++){
+			System.out.println("The value is: "+a[i]);
+		}
+	}
+}
